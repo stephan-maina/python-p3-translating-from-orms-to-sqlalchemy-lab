d6328636ddecca0a6ec080c8c071b0db4fc15988
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 02:01:06 +0300

    completed assignment

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,24 @@
-#!/usr/bin/env python3
-
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
+
+# Database URL
+DATABASE_URL = 'sqlite:///test_db.sqlite'
+
+# Create the database engine
+engine = create_engine(DATABASE_URL)
+
+# Create tables in the database
+Base.metadata.create_all(engine)
+
+# Create a session factory
+Session = sessionmaker(bind=engine)
